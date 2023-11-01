70a1f5e34b72c4076b6255e01af86f780123bfb9
Mulla Ayeesha <ayeeshamulla0708@gmailcom>
2023-11-01 20:13:15 +0530

    Ansible

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 for par in DB_HOST,prod.rds.endpoint ; do
-  aws ssm get-parameter --name $(echo $par | awk -F , '{print $2}') --with-decryption
+  aws ssm get-parameter --name $(echo $par | awk -F , '{print $2}') --with-decryption --query 'Parameter.Value'
 done
