2b882d2753f8111e5edeedf601d42057291a7b6c
Mulla Ayeesha <ayeeshamulla0708@gmailcom>
2023-11-01 20:26:24 +0530

    Ansible

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-for par in DB_HOST,prod.rds.endpoint ; do
+for par in ${INPUT} ; do
   var=$(aws ssm get-parameter --name $(echo $par | awk -F , '{print $2}') --with-decryption --query 'Parameter.Value' --output text)
-  echo export $(echo $par | awk -F , '{print $1}')=$var
+  echo export $(echo $par | awk -F , '{print $1}')=$var >>/config/params
 done
